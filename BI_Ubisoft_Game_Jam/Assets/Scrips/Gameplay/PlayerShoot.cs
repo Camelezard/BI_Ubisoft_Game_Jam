c4c25ff4e1e08b51d7ce984a1affa2f4112c77bc
Joachim-Le <125641749+Joachim-Le@users.noreply.github.com>
2025-11-17 17:39:52 +0100

    JL - canon look at mouse & rotation cap

--- BI_Ubisoft_Game_Jam/Assets/Scrips/Gameplay/PlayerShoot.cs
+++ BI_Ubisoft_Game_Jam/Assets/Scrips/Gameplay/PlayerShoot.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [SerializeField] private LayerMask _layer;
+    [SerializeField] private LayerMask _cameraRayLayer;
+    [SerializeField] private float _maxRotation = 90f;
+    
     public Transform testobject;
     
     private Camera _camera;
@@ -15,6 +17,8 @@ public class PlayerShoot : MonoBehaviour
     void Update()
     {
         Ray lRay = _camera.ScreenPointToRay(Input.mousePosition);
+        float lLimitedAngle;
+        Vector3 lRotation;
         // Vector3 lMousePos = Input.mousePosition;
         // lMousePos.z = Vector3.Distance(_camera.transform.position, transform.position);
         // lMousePos = _camera.ScreenToWorldPoint(lMousePos);
@@ -23,6 +27,13 @@ public class PlayerShoot : MonoBehaviour
         if(Physics.Raycast(lRay, out RaycastHit lHit))
         {
             testobject.position = lHit.point;
+            transform.rotation = Quaternion.LookRotation(
+                Vector3.ProjectOnPlane(lHit.point - transform.position, Vector3.up), Vector3.up);
+                
+            lLimitedAngle = Vector3.SignedAngle(transform.parent.forward, transform.forward, Vector3.up);
+            lLimitedAngle = Mathf.Clamp(lLimitedAngle, -_maxRotation, _maxRotation);
+            lRotation = Quaternion.AngleAxis(lLimitedAngle, Vector3.up) * transform.parent.forward;
+            transform.rotation = Quaternion.LookRotation(lRotation, Vector3.up);
         }
     }
 }
